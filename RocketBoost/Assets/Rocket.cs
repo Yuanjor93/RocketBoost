360bea628232d8193fc39eb85342c120f6a58e9e
Jordan <yuanjor93@gmail.com>
2018-08-19 21:06:34 -0500

    Added Audio and design

---
+++ RocketBoost/Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    static Rigidbody rigidBody;
+    static AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+    }
+
+    private static void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        } else {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A)){
+            rigidBody.transform.Rotate(Vector3.forward);
+        }
+        else   if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.transform.Rotate(-Vector3.forward);
+        }
+	}
+}
